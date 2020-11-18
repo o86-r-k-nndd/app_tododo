35ce8e76e28359c0a41c82b6116ca16999480114
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-18 09:25:52 +0900

    ログインしていない場合はログイン画面へ遷移する様に設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-  #サインアップ時deviseで用いるparamsのデータにUserモデルのカラムを追加する
+  #サインアップ時deviseで用いるparamsのデータにカラムを追加する
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  # ログインしていない場合はログイン画面へ遷移
+  before_action :authenticate_user!
+
   def index
   end
+  
 end
