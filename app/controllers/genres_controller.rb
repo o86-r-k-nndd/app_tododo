c4f2fd3c9f4075c79ba5649ac4a7e2a8a20e669b
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-18 12:43:10 +0900

    一覧表示機能実装

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,20 +1,20 @@
 class GenresController < ApplicationController
   # ログインしていない場合はログイン画面へ遷移
   before_action :authenticate_user!
+  # privateのメソッド
+  before_action :login_user_genre?, only: [:create]
 
   # Top
   def index
+    @genre = Genre.all.order(id: :DESC)
   end
-
   # 新規ジャンル作成
   def new
     @genre = Genre.new
   end
-
   # データベースへ保存
   def create
     @genre = Genre.new(genre_params)
-    binding.pry
     if @genre.save
       redirect_to root_path
     else
@@ -27,6 +27,13 @@ class GenresController < ApplicationController
   # ストロングパラメータ
   def genre_params
     params.require(:genre).permit(:name, :text).merge(user_id: current_user.id)
+  end
+  # 他のユーザーのジャンルの画面へ遷移しようとした時
+  def login_user_genre?
+    @genre = Genre.new(genre_params)
+    if current_user.id == @genre.user_id
+      redirect_to root_path
+    end
   end
 
 end
