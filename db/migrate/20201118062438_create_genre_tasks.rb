9de7d46f5797e81491988becddaf6f8635d36222
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-18 15:36:08 +0900

    migrate task,genre_task

--- db/migrate/20201118062438_create_genre_tasks.rb
+++ db/migrate/20201118062438_create_genre_tasks.rb
@@ -1,7 +1,8 @@
 class CreateGenreTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :genre_tasks do |t|
-
+      t.references :genre
+      t.references :task
       t.timestamps
     end
   end
