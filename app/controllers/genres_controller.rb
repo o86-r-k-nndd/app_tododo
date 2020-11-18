b30ffac75068fe731d4126ed7db7673fa188051a
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-18 11:13:56 +0900

    トップページを変更

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,7 @@
 class GenresController < ApplicationController
+  # ログインしていない場合はログイン画面へ遷移
+  before_action :authenticate_user!
+
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root 'genres#index'
   resources :genres, only: :index
 end
