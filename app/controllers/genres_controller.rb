874ac6f49bdeaa48d9a1833d225f6165a7fcbea2
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-18 11:33:55 +0900

    set routes genres/new, create

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,4 +4,11 @@ class GenresController < ApplicationController
 
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'genres#index'
-  resources :genres, only: :index
+  resources :genres, only: [:index, :new, :create]
 end
