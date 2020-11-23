3e7f40f34501108bad3194ba95a43b8a879e1642
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-23 19:43:54 +0900

    画面遷移処理の実装

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -1,4 +1,7 @@
 class TimersController < ApplicationController
+  # ログインしていない場合はログイン画面へ遷移
+  before_action :authenticate_user!
+
   # 日付選択
   def index
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'genres#index'
+  # 日付
+  resources :timers, only: [:index]
+  get 'today' , to: 'timers#today'
+  get 'tomorrow' , to: 'timers#tomorrow'
+  get 'someday' , to: 'timers#someday'
   resources :genres, only: [:index, :new, :create, :edit, :update, :destroy] do
-    get 'timers', to: 'timers#index'
-    get 'today' , to: 'timers#today'
-    get 'tomorrow' , to: 'timers#tomorrow'
-    get 'someday' , to: 'timers#someday'
     resources :tasks, only: [:index, :new, :create, :edit, :update, :destroy] do
       resources :minitasks, only: [:index, :new, :create, :edit, :update, :destroy]
     end
