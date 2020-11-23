f68074e2c2903ad5d45b3697f24e8ce284c2d471
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-23 17:32:24 +0900

    単体テストコード

--- spec/models/minitask_timer_spec.rb
+++ spec/models/minitask_timer_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe MinitaskTimer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Formオブジェクトのバリデーション' do
+    before do
+      @minitask_timer = FactoryBot.build(:minitask_timer)
+    end
+
+    describe '保存できる時' do
+      it "全ての値が正しく入力されていれば保存できる" do
+        expect(@minitask_timer).to be_valid
+      end
+    end
+    describe '保存できない時' do
+      it "timeが空" do
+        @minitask_timer.time = nil
+        @minitask_timer.valid?
+        expect(@minitask_timer.errors.full_messages).to include("Time can't be blank")
+      end
+      it "nameが空" do
+        @minitask_timer.name = nil
+        @minitask_timer.valid?
+        expect(@minitask_timer.errors.full_messages).to include("Name can't be blank")
+      end
+      it "textが空" do
+        @minitask_timer.text = nil
+        @minitask_timer.valid?
+        expect(@minitask_timer.errors.full_messages).to include("Text can't be blank")
+      end
+      it "task_idが空" do
+        @minitask_timer.task_id = nil
+        @minitask_timer.valid?
+        expect(@minitask_timer.errors.full_messages).to include("Task can't be blank")
+      end
+    end
+
+  end
 end
