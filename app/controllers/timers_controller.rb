5542f8b5fd5ba955fcce8cd78eb488f010b4b818
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-24 08:21:32 +0900

    日付の情報を配列で取得

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -1,6 +1,8 @@
 class TimersController < ApplicationController
   # ログインしていない場合はログイン画面へ遷移
   before_action :authenticate_user!
+  # ミニタスクに紐づいた日付の情報を取得する処理
+  before_action :set_data
 
   # 日付選択
   def index
@@ -13,6 +15,24 @@ class TimersController < ApplicationController
   end
   # いつか
   def someday
+  end
+
+  private
+  # ミニタスクに紐づいた日付の情報を取得する処理
+  def set_data
+    times = []
+    @genres = Genre.where(user_id: current_user.id)
+    @genres.each do |genre|
+      genre.tasks.each do |task|
+        @task_minitasks = TaskMinitask.where(task_id: task.id)
+        @task_minitasks.each do |minitask|
+          @timers = Timer.where(minitask_id: minitask.minitask_id)
+          @timers.each do |timer|
+            times << timer
+          end
+        end
+      end
+    end
   end
 
 end
