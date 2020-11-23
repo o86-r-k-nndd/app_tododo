d079d4ac81ceaeb2081a2cbfd4f4458cd6c202b5
o86-r-k-nndd <on86.kobayashi.ryuuichi@gmail.com>
2020-11-23 18:37:28 +0900

    一覧表示設定

--- app/controllers/timers_controller.rb
+++ app/controllers/timers_controller.rb
@@ -1,3 +1,15 @@
 class TimersController < ApplicationController
-  
+  # 日付選択
+  def index
+  end
+  # 今日
+  def today
+  end
+  # 明日
+  def tomorrow
+  end
+  # いつか
+  def someday
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'genres#index'
   resources :genres, only: [:index, :new, :create, :edit, :update, :destroy] do
+    get 'timers', to: 'timers#index'
+    get 'today' , to: 'timers#today'
+    get 'tomorrow' , to: 'timers#tomorrow'
+    get 'someday' , to: 'timers#someday'
     resources :tasks, only: [:index, :new, :create, :edit, :update, :destroy] do
       resources :minitasks, only: [:index, :new, :create, :edit, :update, :destroy]
     end
